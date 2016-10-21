f05bbae24dbd69069d80071717efcbbf009a9ed1
marioandre0601 <marioandre.06.01@gmail.com>
2016-10-21 16:36:02 -0500

    toggle

--- js/app.js
+++ js/app.js
@@ -31,3 +31,14 @@ function mostrarModal() {
       localStorage.noMostrarModal = true;
    })
 }
+const $filtrosToggle = $('#filtrosToggle')
+
+$filtrosToggle.click(function (ev) {
+   const $i = $filtrosToggle.find('i.fa')
+   const isDown = $i.hasClass('fa-chevron-down')
+   if (isDown){
+      $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+   }else{
+      $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+   }
+})
